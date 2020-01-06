df655c0a52cb3574de794cd7ba4be6801693d281
kritika <kritika.kt.sharma@gmail.com>
2020-01-06 15:06:44 +0530

    commit message

--- OrderService.java
+++ OrderService.java
@@ -1,14 +1,1 @@
-import java.util.ArrayList;
-import java.util.List;
-
-import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
-import com.sap.cloud.sdk.service.prov.api.*;
-import com.sap.cloud.sdk.service.prov.api.annotations.*;
-import com.sap.cloud.sdk.service.prov.api.exits.*;
-import com.sap.cloud.sdk.service.prov.api.request.*;
-import com.sap.cloud.sdk.service.prov.api.response.*;
-import org.slf4j.*;public class OrdersService {
-
-	private static final Logger LOG = CloudLoggerFactory.getLogger(OrdersService.class.getName());
-
-	
+undefined}
